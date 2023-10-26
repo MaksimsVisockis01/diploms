c30b1074058950f30f919332e6aee32e7ec849de
Maksims Visockis <Vezun4ik2004@gmail.com>
2023-10-26 19:11:40 +0300

    working with login & register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,4 +9,17 @@ class RegisterController extends Controller
     public function index(){
         return view('register.index');
     }
+
+    public function store(Request $request){
+        
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $uid = $request->input('uid');
+        $pwd = $request->input('pwd');
+        $pwdrepeat = $request->input('pwdrepeat');
+
+        //dd($request);
+
+        
+    }
 }
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,7 +5,22 @@
 @endsection
 
 @section('content')
-     <p>
-          Login
-     </p>
+
+     <section>
+          <h1>Login</h1>
+          <form action="{{ route('login.store') }}" method="POST">
+              <div class="txt_field">
+                  <input type="text" name="uid" required>
+                  <span></span>
+                  <label>Username</label>
+              </div>
+              <div class="txt_field">
+                    <input type="password" name="pwd" required>
+                        <span></span>
+                        <label>Password</label>
+              </div>
+              <button id="loginButton" type="submit" name="submit">Login</button>
+          </form>
+
+     </section>
 @endsection
